dc790b279b74388095a23ab602d6af7c8f73c4a8
kakeru.t <kakeru.t0227@gmail.com>
2026-05-13 14:05:01 +0900

    first commit

---
+++ config.example.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+  // Mapbox アクセストークン
+  // 取得先: https://account.mapbox.com/access-tokens/
+  mapboxToken: 'YOUR_MAPBOX_ACCESS_TOKEN',
+
+  // Mapbox Studio にアップロードしたタイルセットID（任意）
+  // 例: 'username.xxxxxxxx'
+  // 不要な場合は空文字 '' にしてください
+  tilesetId: '',
+
+  // タイルセットのソースレイヤー名（任意）
+  // Mapbox Studio の Tilesets ページで確認できます
+  tilesetLayerName: '',
+};
